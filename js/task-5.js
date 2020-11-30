a678e2e92dd465af387bfb8043dd92722e82a66e
NEXTru86 <kharinpav86@gmail.com>
2020-12-01 00:03:36 +0300

    Исправил задачу № 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,52 +1,56 @@
 let costOfDelivery;
 
-const china = 'КИТАЙ';
-const chile = 'ЧИЛИ';
-const australia = 'АВСТРАЛИЯ';
-const india = 'ИНДИЯ';
-const jamaica = 'ЯМАЙКА';
+const china = 'китай';
+const chile = 'чили';
+const australia = 'австралия';
+const india = 'индия';
+const jamaica = 'ямайка';
 
 let message;
 
 let country = prompt('Введите страну доставки');
 
-country = country.toUpperCase();
-
-console.log(country);
-
-switch (country) {
-    case china:
-        costOfDelivery = 100;
-        country = 'Китай';
-        message = `Доставка в ${country} будет стоить ${costOfDelivery} кредитов`;
-        break;
-    
-    case chile:
-        costOfDelivery = 250;
-        country = 'Чили';
-        message = `Доставка в ${country} будет стоить ${costOfDelivery} кредитов`;
-        break;
-    
-    case australia:
-        costOfDelivery = 170;
-        country = 'Австралию';
-        message = `Доставка в ${country} будет стоить ${costOfDelivery} кредитов`;
-        break;
-    
-    case india:
-        costOfDelivery = 80;
-        country = 'Индию';
-        message = `Доставка в ${country} будет стоить ${costOfDelivery} кредитов`;
-        break;
-    
-    case jamaica:
-        costOfDelivery = 120;
-        country = 'Ямайку';
-        message = `Доставка в ${country} будет стоить ${costOfDelivery} кредитов`;
-        break;
-    
-    default:
-        message = 'В вашей стране доставка не доступна';
+if (country === null) {
+    alert('Отменено пользователем!');
 }
+else {
+    country = country.toLowerCase();
 
-alert(message);
+    switch (country) {
+        case china:
+            costOfDelivery = 100;
+            country = 'Китай';
+            message = `Доставка в ${country} будет стоить ${costOfDelivery} кредитов`;
+            break;
+        
+        case chile:
+            costOfDelivery = 250;
+            country = 'Чили';
+            message = `Доставка в ${country} будет стоить ${costOfDelivery} кредитов`;
+            break;
+        
+        case australia:
+            costOfDelivery = 170;
+            country = 'Австралию';
+            message = `Доставка в ${country} будет стоить ${costOfDelivery} кредитов`;
+            break;
+        
+        case india:
+            costOfDelivery = 80;
+            country = 'Индию';
+            message = `Доставка в ${country} будет стоить ${costOfDelivery} кредитов`;
+            break;
+        
+        case jamaica:
+            costOfDelivery = 120;
+            country = 'Ямайку';
+            message = `Доставка в ${country} будет стоить ${costOfDelivery} кредитов`;
+            break;
+        
+        default:
+            message = 'В вашей стране доставка не доступна';
+    }
+
+        alert(message);
+
+}
